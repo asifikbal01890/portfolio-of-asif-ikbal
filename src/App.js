d93325eab1daf7fda583cfa803c396977fa0c003
asifilbal01890 <asifikbal01890@gmail.com>
2023-05-22 12:21:33 +0600

    full windows animation complate

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
 import { RouterProvider } from 'react-router-dom';
 import router from './Routes/router/router';
+import BirdsAnimation from './Component/Animations/BridsAnimation/BirdsAnimation';
+import { Canvas } from '@react-three/fiber';
+import FullWindowsAnimation from './Component/Animations/FullWindowsAnimation/FullWindowsAnimation';
 
 function App() {
   return (
-    <div>
+    <div className='bg-[#07060E] h-screen'>
       <RouterProvider
         router={router}
       ></RouterProvider>
+      {/* <Canvas>
+      <ambientLight intensity={0.5} />
+      <pointLight position={[10, 10, 10]} />
+      <BirdsAnimation />
+    </Canvas> */}
+    <FullWindowsAnimation></FullWindowsAnimation>
     </div>
   );
 }
--- src/Component/Home/Home.js
+++ src/Component/Home/Home.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import HeroSection from '../HeroSection/HeroSection';
 
 const Home = () => {
     return (
         <div>
-            hello
+            <HeroSection></HeroSection>
         </div>
     );
 };
