5cf923b19af773bfb953ab9ac6ad2b79a39eae10
asifilbal01890 <asifikbal01890@gmail.com>
2023-06-04 11:16:58 +0600

    navbar complate

--- src/Component/HeroSection/HeroSection.js
+++ src/Component/HeroSection/HeroSection.js
@@ -10,48 +10,59 @@ import mongo from '../../Files/image/logo/MongoDB.png';
 import './HeroSection.css';
 import EllipseTwo from '../../BgLight/EllipseTwo';
 import EllipseThree from '../../BgLight/EllipseThree';
+import { FaFacebook, FaGithub, FaLinkedinIn } from 'react-icons/fa';
 
 const HeroSection = () => {
+
+
     return (
         <div className=''>
             <div className='grid grid-cols-2 items-center mx-auto max-w-[1200px] mt-36'>
-            <div className='text-white font-[Outfit]'>
-                <h1 className='text-5xl'>Hi,</h1>
-                <h1 className='text-5xl'>I am Asif Ikbal</h1>
-            <TypeAnimation
-                sequence={[
-                    // Same String at the start will only be typed once, initially
-                    'I am a MERN Stack Developer',
-                    1000,
-                    'I am a Frontend Developer',
-                    1000,
-                    'I am a React Developer',
-                    1000,
-                    'I am a Full Stack Developer',
-                    1000,
-                ]}
-                speed={50}
-                style={{ fontSize: '2.5em' }}
-                repeat={Infinity}
-            />
-        </div>
-            <div>
-                <div className='flex gap-8 justify-between mx-[-60px]'>
-                    <img className='w-20 animate-img-1 mt-[200px] mb-[-200px]' src={html} alt="" />
-                    <img className='w-16 h-16 animate-img-2 mt-[100px] mb-[-100px]' src={css} alt="" />
-                    <img className='w-14 h-14 animate-img-3 ' src={js} alt="" />
-                    <img className='w-20 animate-img-1' src={react} alt="" />
-                    <img className='w-20 h-16 animate-img-3 mt-[100px] mb-[-100px]' src={node} alt="" />
-                    <img className='w-20 animate-img-2 mt-[200px] mb-[-200px]' src={mongo} alt="" />
+                <div className='text-white font-[Outfit]'>
+                    <h1 className='text-5xl'>Hi,</h1>
+                    <h1 className='text-5xl'>I'm Asif Ikbal</h1>
+                    <span style={{ fontSize: '3em' }}>
+                        <span>I'm</span>{' '}
+                        <TypeAnimation
+                            sequence={[
+                                'Web Developer',
+                                1000,
+                                'MERN Stack Developer',
+                                1000,
+                                'Frontend Developer',
+                                1000,
+                                'React Developer',
+                                1000,
+                            ]}
+                            repeat={Infinity}
+                            speed={1}
+                            deletionSpeed={1}
+                            className={'gradient-text'}
+                        />
+                    </span>
+                    <div className='flex items-center gap-8 text-3xl'>
+                        <p className='gradient-bg rounded-full p-1'><p className='animate-scale'><FaFacebook></FaFacebook></p></p>
+                        <p className='gradient-bg rounded-full p-1'><p className='animate-scale'><FaLinkedinIn></FaLinkedinIn></p></p>
+                        <p className='gradient-bg rounded-full p-1'><p className='animate-scale'><FaGithub></FaGithub></p></p>
+                    </div>
                 </div>
                 <div>
-                    <img className='z-10 sticky rounded-full shadow-[#26a6ba4f] shadow-2xl' src="https://i.ibb.co/4TS4MFX/Pics-Art-12-11-07-14-41.png" alt="" />
+                    <div className='flex gap-8 justify-between mx-[-60px]'>
+                        <img className='w-20 animate-img-1 mt-[200px] mb-[-200px]' src={html} alt="" />
+                        <img className='w-16 h-16 animate-img-2 mt-[100px] mb-[-100px]' src={css} alt="" />
+                        <img className='w-14 h-14 animate-img-3 ' src={js} alt="" />
+                        <img className='w-20 animate-img-1' src={react} alt="" />
+                        <img className='w-20 h-16 animate-img-3 mt-[100px] mb-[-100px]' src={node} alt="" />
+                        <img className='w-20 animate-img-2 mt-[200px] mb-[-200px]' src={mongo} alt="" />
+                    </div>
+                    <div>
+                        <img className='z-10 sticky rounded-full shadow-[#26a6ba4f] shadow-2xl' src="https://i.ibb.co/4TS4MFX/Pics-Art-12-11-07-14-41.png" alt="" />
+                    </div>
                 </div>
             </div>
-        </div>
-        <EllipseOne></EllipseOne>
-        <EllipseTwo></EllipseTwo>
-        <EllipseThree></EllipseThree>
+            <EllipseOne></EllipseOne>
+            <EllipseTwo></EllipseTwo>
+            <EllipseThree></EllipseThree>
         </div>
     );
 };
--- src/Component/NavBar/NavBar.js
+++ src/Component/NavBar/NavBar.js
@@ -1,9 +1,8 @@
 import React, { useState } from 'react';
 import { AiFillCloseCircle } from "react-icons/ai";
-import { FaBlogger, FaCode, FaHome } from "react-icons/fa";
-import { MdFastfood } from "react-icons/md";
+import { FaBlogger, FaCode, FaDownload, FaHome, FaUserAlt } from "react-icons/fa";
+import { IoCall } from "react-icons/io5";
 import { ImMenu } from "react-icons/im";
-import { FiLogIn } from "react-icons/fi";
 import { NavLink } from 'react-router-dom';
 import './NavBar.css';
 
@@ -22,21 +21,16 @@ const NavBar = () => {
          <nav  className={`nav-menu-bar ${open ? "openMenu" : "closeMenu"}`}>
             <ul className='nav-menu' >
 
-               <NavLink to='/' className={`text-2xl ${({isActive})=> isActive ? 'active' : undefined}`} onClick={handleToggle}> <span className='nav-icon'><FaHome/></span><span className='side-text'>Home</span></NavLink>
+               <NavLink to='/' className={`text-lg ${({isActive})=> isActive ? 'active' : undefined}`} onClick={handleToggle}> <span className='nav-icon'><FaHome/></span><span className='side-text'>Home</span></NavLink>
 
-               <NavLink to='/foods' className={`text-2xl ${({isActive})=> isActive ? 'active' : undefined}`} onClick={handleToggle}> <span className='nav-icon'><MdFastfood/></span><span className='side-text'>Foods</span></NavLink>
+               <NavLink to='/about' className={`text-lg ${({isActive})=> isActive ? 'active' : undefined}`} onClick={handleToggle}> <span className='nav-icon'><FaUserAlt/></span><span className='side-text'>About</span></NavLink>
+               <NavLink to='/foods' className={`text-lg ${({isActive})=> isActive ? 'active' : undefined}`} onClick={handleToggle}> <span className='nav-icon'><FaDownload/></span><span className='side-text'>Resume</span></NavLink>
 
-               <NavLink to='/addservice' className={`text-2xl ${({isActive})=> isActive ? 'active' : undefined}`} onClick={handleToggle}><span className='nav-icon'><FaCode/></span><span className='side-text'>Review</span></NavLink>
+               <NavLink to='/skills' className={`text-lg ${({isActive})=> isActive ? 'active' : undefined}`} onClick={handleToggle}><span className='nav-icon'><FaCode/></span><span className='side-text'>Skills</span></NavLink>
 
-               <NavLink to='/blog' className={`text-2xl ${({isActive})=> isActive ? 'active' : undefined}`} onClick={handleToggle}><span className='nav-icon'><FaBlogger/></span><span className='side-text'>Blog</span></NavLink>
+               <NavLink to='/blog' className={`text-lg ${({isActive})=> isActive ? 'active' : undefined}`} onClick={handleToggle}><span className='nav-icon'><FaBlogger/></span><span className='side-text'>Blog</span></NavLink>
 
-               <NavLink to='/login' className={`text-2xl ${({isActive})=> isActive ? 'active' : undefined}`} onClick={handleToggle}><span className='nav-icon'><FiLogIn/></span><span className='side-text'>Login</span></NavLink>
-              {/* {
-                user?.uid ?  <NavLink className={`text-2xl ${({isActive})=> isActive ? 'active' : undefined}`} onClick={handleToggle}{logOut}><span className='nav-icon'><FiLogIn/></span><span className='side-text'>Logout</span></NavLink>
-                :
-                <NavLink to='/login' className={`text-2xl ${({isActive})=> isActive ? 'active' : undefined}`} onClick={handleToggle}><span className='nav-icon'><FiLogIn/></span><span className='side-text'>Login</span></NavLink>
-              } */}
-
+               <NavLink to='/contact' className={`text-lg ${({isActive})=> isActive ? 'active' : undefined}`} onClick={handleToggle}><span className='nav-icon'><IoCall/></span><span className='side-text'>Contact</span></NavLink>
             </ul>
          </nav>
      </>
